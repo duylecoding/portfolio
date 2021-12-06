dbc5ad8cf64bf6dcca9483b38fdf512e62208f5e
Duy Le <duylezero@gmail.com>
2021-12-06 13:16:50 -0600

    basic header and fotoer, router

--- src/index.js
+++ src/index.js
@@ -1,14 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import { render } from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./pages/header/header";
+import Footer from "./pages/footer/footer";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const rootElement = document.getElementById("root");
+render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/header" element={<Header />} />
+      <Route path="/footer" element={<Footer />} />
+    </Routes>
+  </BrowserRouter>,
+  rootElement
 );
 
 // If you want to start measuring performance in your app, pass a function
---
+++ src/pages/footer/footer.jsx
@@ -0,0 +1,5 @@
+function Footer({}) {
+  return <h1>footer</h1>;
+}
+
+export default Footer;
