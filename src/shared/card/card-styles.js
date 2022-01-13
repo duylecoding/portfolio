8d98e12a2d5fc9e98e8b91eb5b4dffd9a6dfb4b3
Duy Le <duylezero@gmail.com>
2022-01-13 11:09:58 -0600

    update colors and stuff

--- src/shared/card/card-styles.js
+++ src/shared/card/card-styles.js
@@ -2,6 +2,7 @@ import { Flex } from "@chakra-ui/layout";
 import styled from "styled-components";
 import { B_GRAY, PRIMARY } from "../colors";
 import { SECONDARY_PRINT } from "../typefaces";
+import { MEDIUM } from "../screensizes";
 
 export const Container = styled(Flex)`
   justify-content: center;
@@ -23,6 +24,10 @@ export const Container = styled(Flex)`
   transition: 0.5s;
   &:hover {
     box-shadow: 0 12px 16px 0 rgba(0, 0, 0, 0.6);
-    margin-top: -4px;
+    margin-top: -2px;
+  }
+  @media (max-width: ${MEDIUM}px) {
+    font-size: 32px;
   }
 `;
+y;
